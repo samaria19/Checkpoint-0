b2b4b991f6ded147fddcf08dc87418186b2db588
Jonathan Corley <jcorley@noscihigh.org>
2021-01-14 20:46:46 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// Strings
+
+/* 
+First, declare variable named firstName.
+
+Assign firstName to a string value of your first name.
+
+Declare another variable named lastName and assign it the value of your last name.
+
+Then declare another variable named fullName and assign it to the current value of firstName and lastName and have the joined values separated with a space character.
+Remember that computer languages will not add the space character for you so how would you do that manually?
+*/
+
+
+
+
+//Numbers
+/*
+Declare a variable named luckyNumber.
+Assign luckyNumber to a number of your choice.
+Declare another variable named luckyNumberTimes2 and assign it the value of luckyNumber multiplied by 2.
+*/
+
+
+//Booleans
+/*
+Declare a variable named myBool and assign it the value of true.
+Then, declare another named isEqualTo and assign it the value of strictly comparing luckyNumber and luckyNumberTimes2.
+*/
